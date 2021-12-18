f26f77e765168f62a3e4484c99ae27d1f928dd0c
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2021-12-18 20:35:52 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,31 @@
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.losses import DiceLoss, DiceCELoss
+
+import torch
+from preporcess import prepare
+from utilities import train
+
+
+data_dir = 'D:/Youtube/Organ and Tumor Segmentation/datasets/Task03_Liver/Data_Train_Test'
+model_dir = 'D:/Youtube/Organ and Tumor Segmentation/results/results' 
+data_in = prepare(data_dir, cache=True)
+
+device = torch.device("cuda:0")
+model = UNet(
+    dimensions=3,
+    in_channels=1,
+    out_channels=2,
+    channels=(16, 32, 64, 128, 256), 
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+    norm=Norm.BATCH,
+).to(device)
+
+
+#loss_function = DiceCELoss(to_onehot_y=True, sigmoid=True, squared_pred=True, ce_weight=calculate_weights(1792651250,2510860).to(device))
+loss_function = DiceLoss(to_onehot_y=True, sigmoid=True, squared_pred=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-5, weight_decay=1e-5, amsgrad=True)
+
+if __name__ == '__main__':
+    train(model, data_in, loss_function, optimizer, 600, model_dir)
